843137a0b392c4352aa5fde3adc7f8996ea617d0
Alejandro <alejandrovalenzuela051@gmail.com>
2021-01-31 10:37:16 -0600

    Corrections and getType and clear added

--- Dinamic array/DinArray.py
+++ Dinamic array/DinArray.py
@@ -24,6 +24,15 @@ class DinArray:
 
     def set(self, val, index):
         if isinstance(val, self._data_type):
-            self._array[index]
+            self._array[index] = val
         else:
             raise TypeError(f"Data type of {val} different to array")
+
+    def getType(self):
+        return self._data_type
+
+    def clear(self):
+        curr_ind = 0
+        while curr_ind < self._capacity:
+            self._array[curr_ind] = None
+            curr_ind += 1
--- Dinamic array/test.py
+++ Dinamic array/test.py
@@ -1,11 +1,12 @@
 from DinArray import DinArray
 
 array = DinArray(capacity=5, data_type=bool)
-print(f"Created array of capacity {array.size()}")
+print(f"Created array of capacity {array.size()} \nData type {array.getType()}")
 
-print(f"\n is it Empty? {array.isEmpty()}")
-
-print(f"\n Value at index 4: {array.get(4)}")
+print(f"\n is it Empty? {array.isEmpty()} \n Value at index 4: {array.get(4)}")
 
 array.set(True, 0)
 print("\n New value 'True' at index 0")
+
+array.clear()
+print("All values set to 'None' or 'Null' for the non-pythonists")
