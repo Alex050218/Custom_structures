4c1109bbeb513e7cab491b5486a2e5815a958ea6
Alejandro <alejandrovalenzuela051@gmail.com>
2021-01-31 11:40:09 -0600

    New test format

--- Dinamic array/test.py
+++ Dinamic array/test.py
@@ -1,12 +1,19 @@
 from DinArray import DinArray
 
 array = DinArray(capacity=5, data_type=bool)
-print(f"Created array of capacity {array.size()} \nData type {array.getType()}")
 
-print(f"\n is it Empty? {array.isEmpty()} \n Value at index 4: {array.get(4)}")
+info = f"""\
+Created array of capacity {array.size()}
+Data type {array.getType()} \n"""
+print(info)
+
+values = f"""\
+is it Empty? {"Yes" if array.isEmpty() == 0 else "No"}
+Value at index 4: {array.get(4)} \n"""
+print(values)
 
 array.set(True, 0)
-print("\n New value 'True' at index 0")
+print("New value 'True' at index 0 \n")
 
 array.clear()
-print("All values set to 'None' or 'Null' for the non-pythonists")
+print("All values deleted from the array and size set to 0")
