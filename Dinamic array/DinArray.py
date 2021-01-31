d0128b87dda8f4579674711721d7b3b5119ba8e8
Alejandro <alejandrovalenzuela051@gmail.com>
2021-01-31 09:30:10 -0600

    Privacy improved

--- Dinamic array/DinArray.py
+++ Dinamic array/DinArray.py
@@ -1,13 +1,14 @@
 class DinArray:
     _array = []
-    capacity = 0
-    data_type = None
+    _capacity = 0
+    _data_type = None
 
     def __init__(self, capacity, data_type):
         if capacity <= 0:
             raise ValueError("Capacity must be 1 or more")
 
-        self.data_type = data_type
+        self._data_type = data_type
+        self._capacity = capacity
 
         for _ in range(capacity):
             self._array.append(None)
