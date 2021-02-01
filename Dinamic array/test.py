415318d733102648e42fb3f6de809684ec9b9ac8
Alejandro <alejandrovalenzuela051@gmail.com>
2021-02-01 14:55:56 -0600

    New file test format

--- Dinamic array/test.py
+++ Dinamic array/test.py
@@ -1,41 +1,61 @@
 from DinArray import DinArray
 
+
+def status_array(ar):
+    if not isinstance(ar, DinArray):
+        raise TypeError("Not a dinamic array")
+
+    print(
+        f"""
+    New array: {ar.get_string()}
+    New capacity: {ar.capacity()}
+    New lenght: {ar.get_len()} \n"""
+    )
+
+
 array = DinArray(capacity=5, data_type=bool)
 
-info = f"""\
-Created array of capacity {array.capacity()} \n
-Data type {array.get_type()} \n"""
-print(info)
+print(
+    f"""\
+Created array of capacity {array.capacity()}
 
-print(f'is it Empty? {"Yes" if array.is_empty() else "No"} \n')
+Data type {array.get_type()}
+
+is it Empty? {"Yes" if array.is_empty() else "No"}"""
+)
 
 array.set_at(4, True)
-print("New value 'True' at index 4 \n")
+print(
+    f"""
+New value 'True' at index 4
 
-print(f"Amount of values introduced into the array by the user: {array.get_len()}\n")
+Amount of values introduced into the array by the user: {array.get_len()}
 
-print(f"String of the created array: {array.get_string()} \n")
+String of the created array: {array.get_string()}
 
-print("Iteration of all values in the array:")
+Iteration of all values in the array:"""
+)
+
 for val in iter(array):
     print(f"\t {val}")
 
-print("Representation of the array :", array, "\n")
+print("Representation of the array :", array)
 
-print(
-    f"is within the array a true value?: {'Yes' if array.contains(True) else 'No'} \n"
-)
-
-print(f"The index of true is: {array.index_of(True)} \n")
-
-print("Removing true from the array...")
-array.remove_at(4)
 print(
     f"""
-    New array: {array.get_string()}
-    New capacity: {array.capacity()}
-    New lenght: {array.get_len()}"""
+is within the array a true value?: {'Yes' if array.contains(True) else 'No'}
+
+The index of true is: {array.index_of(True)}
+
+Removing true from the array..."""
 )
+
+array.remove_at(4)
+status_array(array)
+
+print("Removing the first instance of 'None'...")
+array.remove(None)
+status_array(array)
 
 array.clear()
 print("All values deleted from the array and size set to its respective default value")
