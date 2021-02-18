0c793099a8f38e72833cec365b5707d0a162640d
Alejandro <alejandrovalenzuela051@gmail.com>
2021-02-18 12:56:37 -0600

    Created method for checking new nodes

--- Double linked list/DlList.py
+++ Double linked list/DlList.py
@@ -1,5 +1,15 @@
-class Node:
+class DNode:
     def __init__(self):
         self.data = None
         self.next = None
         self.prev = None
+
+
+class DoLList:
+    def __init__(self):
+        self._lenght = 0
+        self._head = None
+
+    def _check_DNode(self, new_node):
+        if not isinstance(new_node, DNode):
+            raise TypeError
