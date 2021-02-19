91a8184d22c009f74f06a8c6acd99c05bb5ffbcc
Alejandro <alejandrovalenzuela051@gmail.com>
2021-02-18 18:38:40 -0600

    Test file format changed

--- Double linked list/test.py
+++ Double linked list/test.py
@@ -5,13 +5,13 @@ Llist = DoLList()
 for val in range(10):
     Llist.At_beggining(DNode(val))
 
-print(f"New Double Linked List: {Llist.get_str()}")
+print(f"New Double Linked List: {Llist.get_str()} \n")
 
 Llist.At_end(DNode(-1))
-print(f"New value at end of the list: {Llist}")
+print(f"New value at end of the list: {Llist} \n")
 
 Llist.insert_at(0, DNode(-2))
-print(f"New value at index 0: {Llist}")
+print(f"New value at index 0: {Llist} \n")
 
 search_index = 8
 Llist.insert_at(search_index, DNode(-3))
