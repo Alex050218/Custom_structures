c4d94a95be2b537135b01adeedb269232255703a
Alejandro <alejandrovalenzuela051@gmail.com>
2021-03-14 16:11:07 -0600

    get top and pop method created

--- Structures/Stack.py
+++ Structures/Stack.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
+from typing import Any
 from Structures.LList import LinkList, Node
 
 b = 2
 
 class StackS(LinkList):
-    def __init__(self, new_head: Node | None = None):
+    def __init__(self, new_head: Node | None = None) -> None:
         super().__init__(new_head=new_head)
 
         if new_head is not None:
@@ -12,5 +13,12 @@ class StackS(LinkList):
         else:
             self._head = Node()
 
-    def push(self, new_node: Node):
+    def push(self, new_node: Node) -> None:
         self.InsertAt(0,new_node)
+
+    def get_top(self) -> Any:
+        return self.get_data(0)
+
+    def pop(self) -> Any:
+        old_head = self.removeAt(0)
+        return old_head
--- tests/test_StackStructure.py
+++ tests/test_StackStructure.py
@@ -11,3 +11,11 @@ print(f"New linked list: {test_stack}")
 
 test_stack.push(Node(-1))
 print(f"New head: {test_stack}")
+
+print(f"Current head: {test_stack.get_top()}")
+
+old_head: int = test_stack.pop()
+print(f"Popped value {old_head}")
+
+print(f"Current stack: {test_stack}")
+print(f"Current lenght: {test_stack.Lenght()}")
